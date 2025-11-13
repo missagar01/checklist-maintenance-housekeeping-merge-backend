1210413a6e2065a0d0e619791c48bde2d1679e41
vikas choudhary <vikaschoudhary@vikass-MacBook-Air.local>
2025-11-13 13:27:49 +0530

    some update

--- server.js
+++ server.js
@@ -47,4 +47,6 @@ app.use("/api/machine-details", machineDetailsRoutes);
 
 
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+
