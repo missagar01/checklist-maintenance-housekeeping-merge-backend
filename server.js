cc34f0b494a876b66849631a94b56c6d3881236d
tuleshwar <team1.interns@botivate.in>
2026-03-16 10:28:42 +0530

    updated error

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import compression from "compression";
 
 
 dotenv.config();
 const app = express();
 const DEPLOY_MODE = process.env.DEPLOY_MODE === "true";
 
+app.use(compression());
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
